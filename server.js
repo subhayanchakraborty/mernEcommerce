b364cdd6e532a5908d3237078170f23caf683186
subhayanchakraborty <97455196+subhayanchakraborty@users.noreply.github.com>
2023-07-21 01:11:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,16 @@ import categoryRoutes from "./routes/categoryRoute.js"
 import productRoutes from "./routes/productRoute.js"
 import cors from "cors";
 import path from "path";
+import {fileURLToPath} from 'url';
 
 //CONFIGURE DOTENV
 dotenv.config();
 //DB CONNECTION
 connectDB();
 
+//esmodule fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 const app=express();
 
 //MIDDLEWARES
